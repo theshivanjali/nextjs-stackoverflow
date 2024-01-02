e1b884382072f358ba6ff147e706b754948fa7a5
Shivanjali Chaurasia <shivanjali012@gmail.com>
2024-01-02 13:18:28 +0530

    Authentication Added

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
 
-const Home = () => {
+export default function Home() {
   return (
-    <div>Home</div>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
-
-export default Home
